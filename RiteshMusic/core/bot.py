c63abaf51df0e2ff5b40b192a8e341fe116b9f6d
Yadavperson <shyamsundar12345676@gmail.com>
2025-07-25 02:57:18 +0530

    Update bot.py

--- RiteshMusic/core/bot.py
+++ RiteshMusic/core/bot.py
@@ -14,7 +14,7 @@ class Anony(Client):
     def __init__(self):
         LOGGER(__name__).info("🛠️ Initializing RiteshMusic Bot...")
         super().__init__(
-            name="DeadlineTech",
+            name="AuraVisual",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
